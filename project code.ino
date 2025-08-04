605ae5a09d14dbfebf6b0df61d8be5f79364ed73
tarun-singh9897 <tarun.singh84494@gmail.com>
2025-08-04 17:45:31 +0530

    Add files via upload

---
+++ project code.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  for (int pin = 0; pin <= 13; pin++) {
+    digitalWrite(pin, HIGH);
+    delay(600);
+    digitalWrite(pin, LOW);
+    delay(600);
+  }
+}
+
+
